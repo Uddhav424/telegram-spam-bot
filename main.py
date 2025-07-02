d71ad6027ed6d4c70b85dfa7d3823213edb35a07
Uddhav424 <uddhavthakur2411@gmail.com>
2025-07-02 16:31:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 
+from telethon.sync import TelegramClient
+import time
+import os
+
+api_id = int(os.environ['API_ID'])
+api_hash = os.environ['API_HASH']
+session = os.environ['SESSION_NAME']
+target = os.environ['TARGET_USERNAME']
+message = os.environ['SPAM_MESSAGE']
+count = int(os.environ['SPAM_COUNT'])
+
+client = TelegramClient(session, api_id, api_hash)
+
+with client:
+    for i in range(count):
+        client.send_message(target, message)
+        print(f"✅ Sent message #{i+1}")
+        time.sleep(2)
